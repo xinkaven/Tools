be9b10e15ec6c8fd7f9808793f9e1acc106207be
xinkaven <274105051@qq.com>
2022-08-16 17:53:08 +0800

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,31 @@
+
+
+#!/bin/sh
+#
+#脚本功能描述：依据/proc/stat文件获取并计算CPU使用率
+#
+#CPU时间计算公式：CPU_TIME=user+system+nice+idle+iowait+irq+softirq
+#CPU使用率计算公式：cpu_usage=(idle2-idle1)/(cpu2-cpu1)*100
+#默认时间间隔
+TIME_INTERVAL=5
+while(true)
+do
+    time=$(date "+%Y-%m-%d %H:%M:%S") 
+	timefile=$(date "+%Y-%m-%d")
+	LAST_CPU_INFO=$(cat /proc/stat | grep -w cpu | awk '{print $2,$3,$4,$5,$6,$7,$8}')
+	LAST_SYS_IDLE=$(echo $LAST_CPU_INFO | awk '{print $4}')
+	LAST_TOTAL_CPU_T=$(echo $LAST_CPU_INFO | awk '{print $1+$2+$3+$4+$5+$6+$7}')
+	sleep ${TIME_INTERVAL}
+	NEXT_CPU_INFO=$(cat /proc/stat | grep -w cpu | awk '{print $2,$3,$4,$5,$6,$7,$8}')
+	NEXT_SYS_IDLE=$(echo $NEXT_CPU_INFO | awk '{print $4}')
+	NEXT_TOTAL_CPU_T=$(echo $NEXT_CPU_INFO | awk '{print $1+$2+$3+$4+$5+$6+$7}')
+
+	#系统空闲时间
+	SYSTEM_IDLE=`echo ${NEXT_SYS_IDLE} ${LAST_SYS_IDLE} | awk '{print $1-$2}'`
+	#CPU总时间
+	TOTAL_TIME=`echo ${NEXT_TOTAL_CPU_T} ${LAST_TOTAL_CPU_T} | awk '{print $1-$2}'`
+	CPU_USAGE=`echo ${SYSTEM_IDLE} ${TOTAL_TIME} | awk '{printf "%.2f", 100-$1/$2*100}'`
+
+	echo "CPU Usage:${CPU_USAGE}%" $time >> ./cpu_${timefile}.log
+	sleep ${TIME_INTERVAL}
+done
